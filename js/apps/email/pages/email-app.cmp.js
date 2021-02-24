2368edacee4c5024aa8f57206977dff4c1ccc4a9
Your Name <your_address_email@domain.com>
2021-02-24 12:55:26 +0200

    in localstoarge

--- js/apps/email/pages/email-app.cmp.js
+++ js/apps/email/pages/email-app.cmp.js
@@ -1,12 +1,34 @@
+import { emailService } from '../services/email.service.js';
+import emailList from '../cmps/email-list.cmp.js';
+
 export default {
+    name:'emailApp',
     template: `
-    <section class= "book-app">
-        <!-- <book-filter @filtered="setFilter" />
-        <book-add @bookAdiding="loadBooks" />
-        <book-list :books="booksToShow" @remove="removeBook"/>    -->
-        <h1>ho</h1>
+    <section class= "main-app">
+
+        <h1 class="">email-app</h1>
 
     </section>
 
     `,
-}
+  data() {
+    return {
+      emails: emailService.query(),
+      selecetMail: null,
+      filterBy: null,
+    };
+  },
+  methods: {
+    test() {
+      const test = emailService.query().then((emails) => (this.emails = emails));
+      
+
+    },
+  },
+  components: {
+    emailList,
+  },
+  created(){
+      this.test()
+  }
+};
