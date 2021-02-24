5af741b560c35f8987e4de42b36ec052862bd5f0
Your Name <your_address_email@domain.com>
2021-02-24 16:44:38 +0200

    route

--- js/apps/email/cmps/email-list.cmp.js
+++ js/apps/email/cmps/email-list.cmp.js
@@ -5,15 +5,13 @@ export default {
   template: `
     <ul class="email-list">
     <li v-for="email in emails" :key="email.id" class="email-preview-container">
-               <email-preview :email="email"/>
-               <!-- <router-link class="open-preview-btn" :to="'/book/'+book.id"> Details</router-link> -->
+    <router-link class="open-mail-btn" :to="'/email/'+email.id">  <email-preview :email="email"/>  </router-link>
+          
     </li>
     </ul>
     `,
   methods: {
-    select(book) {
-      this.$emit('selected', book);
-    },
+  
   },
   components: {
     emailPreview,
