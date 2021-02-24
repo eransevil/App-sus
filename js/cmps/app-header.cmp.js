0d8d1d5c9db6163cd0a5158a502d6ccc659f8d6f
Your Name <your_address_email@domain.com>
2021-02-24 20:16:52 +0200

    nav bar

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -1,14 +1,12 @@
 export default {
-    template: `
+  template: `
    <header class="app-header flex align-center space-between">
-        <h1> App Sus</h1>
-        <!-- <nav class="nav-bar">
-           <router-link active-class="active-link" to="/" exact>Home</router-link> |
-           <router-link to="/book">Books</router-link> |
-           <router-link to="/about">About</router-link>
-
-       </nav> -->
+   <router-link active-class="active-link" to="/" exact><h1> Home </h1> </router-link>
+        <nav class="nav-bar">
+           <router-link to="/keep">Keep</router-link> |
+           <router-link to="/email">Email</router-link>
+           <router-link to="/book">Book</router-link>
+       </nav>
     </header>
     `,
-
-}
+};
