6a49a2a2f15b5b5df4b88df969364f16fbfcfd38
Your Name <your_address_email@domain.com>
2021-02-25 22:03:46 +0200

    css

--- js/apps/email/cmps/email-list.cmp.js
+++ js/apps/email/cmps/email-list.cmp.js
@@ -2,13 +2,15 @@ import emailPreview from '../cmps/email-preview.cmp.js';
 import { emailService } from '../services/email.service.js';
 
 export default {
-  props: ['emails'], 
+  props: ['emails'],
   template: ` 
       <ul class="scroll-container">
       <li v-for="email in emails" :key="email.id" class="email-preview-container">
-         <email-preview @click.native="renderPreview(email.id)"  @markRead="mark"  @starredToggle="toggleStar"  :email="email"/>  
+         <email-preview @click.native="renderPreview(email.id)"  @markRead="mark"  @starredToggle="toggleStar"  :email="email"/> 
+         <div class="read-delete-btn-container"> 
         <div title="Mark As UnRead"  @click="MarkUnRead(email.id)" class="unread-btn"> 📩 </div>
         <div title="Delete"  @click="showDeleteModal(email.id)" class="delete-btn"> ✖ </div>
+      </div>
       </li>
     </ul>
     `,
@@ -16,8 +18,6 @@ export default {
     toggleStar(emailId) {
       console.log(emailId);
       this.$emit('starredToggle', emailId);
-      
-      
     },
     showDeleteModal(emailId) {
       Swal.fire({
@@ -45,12 +45,10 @@ export default {
       this.$emit('UnRead', id);
     },
 
-    renderPreview(emailId){
+    renderPreview(emailId) {
       this.$router.push(`/email/${emailId}`);
-    }
+    },
   },
-
-
 
   components: {
     emailPreview,
